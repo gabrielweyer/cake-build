a3cc1f934061c39f7a061343c80756c2c73f71bd
Gabriel Weyer <gabrielweyer@gmail.com>
2017-12-28 11:42:03 +1100

    Fix compilation but break test
    I want to see how Travis and AppVeyor behave

--- src/SuperLogic/EvenMoreUseless.cs
+++ src/SuperLogic/EvenMoreUseless.cs
@@ -7,7 +7,7 @@ namespace Contoso.Hello.SuperLogic
     {
         public static string DoWork()
         {
-            return IncrediblyUsless.DoNothing($"Some JSON (maybe): {ReallyUseless.SayHi()}");
+            return IncrediblyUseless.DoNothing($"Some JSON (maybe): {ReallyUseless.SayHi()}");
         }
     }
 }
