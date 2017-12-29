48529fb649a34ad5aecbd982317b1fec25b1cfc4
Gabriel Weyer <gabrielweyer@gmail.com>
2017-12-29 16:20:27 +1100

    Remove colour from dotnet install script

--- tools/dotnet-install.sh
+++ tools/dotnet-install.sh
@@ -12,7 +12,7 @@ set -u
 set -o pipefail
 
 # Use in the the functions: eval $invocation
-invocation='say_verbose "Calling: ${yellow:-}${FUNCNAME[0]} ${green:-}$*${normal:-}"'
+invocation='say_verbose "Calling: ${FUNCNAME[0]} $*"'
 
 # standard output may be used as a return value in the functions
 # we need a way to write text on the screen in the functions so that
@@ -20,33 +20,14 @@ invocation='say_verbose "Calling: ${yellow:-}${FUNCNAME[0]} ${green:-}$*${normal
 # Exposing stream 3 as a pipe to standard output of the script itself
 exec 3>&1
 
-# Setup some colors to use. These need to work in fairly limited shells, like the Ubuntu Docker container where there are only 8 colors.
-# See if stdout is a terminal
-if [ -t 1 ]; then
-    # see if it supports colors
-    ncolors=$(tput colors)
-    if [ -n "$ncolors" ] && [ $ncolors -ge 8 ]; then
-        bold="$(tput bold       || echo)"
-        normal="$(tput sgr0     || echo)"
-        black="$(tput setaf 0   || echo)"
-        red="$(tput setaf 1     || echo)"
-        green="$(tput setaf 2   || echo)"
-        yellow="$(tput setaf 3  || echo)"
-        blue="$(tput setaf 4    || echo)"
-        magenta="$(tput setaf 5 || echo)"
-        cyan="$(tput setaf 6    || echo)"
-        white="$(tput setaf 7   || echo)"
-    fi
-fi
-
 say_err() {
-    printf "%b\n" "${red:-}dotnet_install: Error: $1${normal:-}" >&2
+    printf "%b\n" "dotnet_install: Error: $1" >&2
 }
 
 say() {
     # using stream 3 (defined in the beginning) to not interfere with stdout of functions
     # which may be used as return value
-    printf "%b\n" "${cyan:-}dotnet-install:${normal:-} $1" >&3
+    printf "%b\n" "dotnet-install: $1" >&3
 }
 
 say_verbose() {
